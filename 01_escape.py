295af635064a3384219c4b9926c363d80e947da4
JongSung Bae <40561016+jsbae21@users.noreply.github.com>
2019-11-09 00:35:29 +0900

    escape.py

---
+++ 01_escape.py
@@ -0,0 +1,23 @@
+'''
+2019.11.08_이스케이프 문자
+\n : 엔터, 줄바꿈
+\t : tab
+\\ : \를 표현하고 싶을때 사용
+\' : 작은 따옴표 표현
+\" : 큰 따옴표 표현
+'''
+print('python start') #작은 따옴표로 둘러싸기
+print("python start") #큰 따옴표로 둘러싸기
+print()
+print('welcome to\n' 'python\n' 'world')
+print('welcome to\t' 'python world')
+print('welcome to\\' 'python world')
+print()
+print("Welcome to 'python' world")
+print('"welcome to python world!" he says')
+print("Welcome to \"python\" world")
+print('\'welcome to python world!\' he says')
+print()
+print('Welcome', end=' ')
+print('to', end=' ')
+print('python world', end=' ')
