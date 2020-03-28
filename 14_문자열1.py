841625986d7406fcd0d8e857115d60a3eb7fbed4
Jace.Bae <baelong86@gmail.com>
2020-03-27 22:46:10 -0400

    string

--- 14_문자열1.py
+++ 14_문자열1.py
@@ -19,15 +19,6 @@ print(str(10.1), type(str(10.1)))
 print(str(True), type(str(True)))
 print()
 
-# 문자열 함수(upper, isalnum, startswith, count, endswith ...)
-print("Capitalize : ", str_o1.capitalize()) # 첫글자 대문자로 변환
-print("endswith : ", str_o2.endswith("s")) # 마지막 문자가 무엇인지 체크
-print("replace : ", str_o1.replace("Nice", 'Good'))
-print("replace : ", str_o1.replace("thon", ' Good')) # 글자 변환
-print("sorted : ", sorted(str_o1)) # 리스트 형태로 반환, 순서가 정렬되서 출력
-print("split : ", str_o4.split(' ')) # 공백을 분리, !라면 !를 기준으로 분리
-print()
-
 # 반복(시퀀스) : 순서가 있는 배열 형태
 im_str = "Good Boy!"
 print(dir(im_str)) # _iter_ 요게있으면 반복할 수 있다.
