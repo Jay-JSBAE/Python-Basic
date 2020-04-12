6525299064fe727c57fd25461337653f228c3f73
Jace.Bae <baelong86@gmail.com>
2020-04-12 13:04:31 -0400

    Create 36_turtle_graphics.py

---
+++ 36_turtle_graphics.py
@@ -0,0 +1,25 @@
+# 파이썬에서 그림을 그리는 작업을 지원하는 모듈
+# 그림을 그리는 방법은 turtle 모션을 사용하는 방법관 pen 컨트롤을 사용하는 방법이 있다.
+'''
+turtle.showturtle()
+turtle.hideturtle()
+turtle.penup()
+turtle.pendown()
+turtle.goto(좌표값)
+turtle.done()
+turtle.exitonclick()
+'''
+
+import turtle
+
+t = turtle.Pen()
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
